c4179dff78d290e23e822862e5c3952377e8d142
Neil Vodoor <nvodoor@gmail.com>
2017-03-21 19:41:30 -0700

    Updated Round Counter to properly work.

--- Client/src/index.jsx
+++ Client/src/index.jsx
@@ -8,17 +8,26 @@ class Game extends React.Component {
 		this.state = {
 			player: '',
 			comp: '',
-			moves: ['rock', 'paper', 'scissor']
+			moves: ['rock', 'paper', 'scissor'],
+      rounds: 0
 		}
 	}
 
 	handleSubmit() {
 		const move = this.state.moves;
 		const play = Math.floor(Math.random()*move.length);
-		this.setState({
-			comp: move[play]
-		})
+    this.setState({
+      comp: move[play],
+    })
 	}
+
+  handleReRender() {
+    this.setState({
+      player: '',
+      comp: '',
+      rounds: this.state.rounds + 1
+    })
+  }
 
 
   render () {
@@ -31,53 +40,83 @@ class Game extends React.Component {
 		<input type="submit" value="Scissor" onClick={() => this.setState({player: 'scissor'})}/>
 		<input type="submit" value="Paper" onClick={() => this.setState({player: 'paper'})}/>
 		</div>
+    <div>
 		<button onClick={this.handleSubmit.bind(this)}>Click to Play</button>
+    </div>
+    <h4>Rounds Played: {this.state.rounds} </h4>
 		</div>
 	)
   	} 
   	if (this.state.player === 'rock' && this.state.comp === 'rock') {
   		return (
+      <div>
   		<div> Player chose: {this.state.player}. Computer chose: {this.state.comp} Tied! </div>
+      <button onClick={this.handleReRender.bind(this)}>Click to Play Again!</button>
+      </div>
   		)
   	}
   	if (this.state.player === 'rock' && this.state.comp === 'paper') {
   		return (
-  		<div> Player chose: {this.state.player}. Computer chose: {this.state.comp}. Computer won! </div>
+      <div>
+  		<div> Player chose: {this.state.player}. Computer chose: {this.state.comp}. Computer won!</div>
+      <button onClick={this.handleReRender.bind(this)}>Click to Play Again!</button>
+      </div>
   		)
   	}
   	if (this.state.player === 'rock' && this.state.comp === 'scissor') {
   		return (
+      <div>
   		<div> Player chose: {this.state.player}. Computer chose: {this.state.comp}. Player won! </div>
+      <button onClick={this.handleReRender.bind(this)}>Click to Play Again!</button>
+      </div>
   		)
   	}
   	if (this.state.player === 'scissor' && this.state.comp === 'rock') {
   		return (
+      <div>
   		<div> Player chose: {this.state.player}. Computer chose: {this.state.comp}. Computer won! </div>
+      <button onClick={this.handleReRender.bind(this)}>Click to Play Again!</button>
+      </div>
   		)
   	}
   	if (this.state.player === 'scissor' && this.state.comp === 'paper') {
   		return (
+      <div>
   		<div> Player chose: {this.state.player}. Computer chose: {this.state.comp}. Player won! </div>
+      <button onClick={this.handleReRender.bind(this)}>Click to Play Again!</button>
+      </div>
   		)
   	}
   	if (this.state.player === 'scissor' && this.state.comp === 'scissor') {
   		return (
+      <div>
   		<div> Player chose: {this.state.player}. Computer chose: {this.state.comp}. Tied! </div>
+      <button onClick={this.handleReRender.bind(this)}>Click to Play Again!</button>
+      </div>
   		)
   	}
   	if (this.state.player === 'paper' && this.state.comp === 'rock') {
   		return (
+      <div>
   		<div> Player chose: {this.state.player}. Computer chose: {this.state.comp}. Player won! </div>
+      <button onClick={this.handleReRender.bind(this)}>Click to Play Again!</button>
+      </div>
   		)
   	}
   	if (this.state.player === 'paper' && this.state.comp === 'paper') {
   		return (
+      <div>
   		<div> Player chose: {this.state.player}. Computer chose: {this.state.comp}. Tied! </div>
+      <button onClick={this.handleReRender.bind(this)}>Click to Play Again!</button>
+      </div>
   		)
   	}
   	if (this.state.player === 'paper' && this.state.comp === 'scissor') {
   		return (
+      <div>
   		<div> Player chose: {this.state.player}. Computer chose: {this.state.comp}. Computer won! </div>
+      <button onClick={this.handleReRender.bind(this)}>Click to Play Again!</button>
+      </div>
   		)
   	}
   }
